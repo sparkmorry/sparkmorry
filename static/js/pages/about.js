2d98a9e26f7cd1d6c92c9b72a55ac2254dbd1e6b
Mengli Xia <morryxia@gmail.com>
2016-03-03 22:58:03 +0800

    轮播画集相册

---
+++ static/js/pages/about.js
@@ -0,0 +1,11 @@
+var galleryTop = new Swiper('#j-gallery', {
+        nextButton: '#j-gallery-next',
+        prevButton: '#j-gallery-prev',
+        spaceBetween: 10,
+});
+
+var album = new Swiper('#j-album', {
+        nextButton: '#j-album-next',
+        prevButton: '#j-album-prev',
+        spaceBetween: 10,
+});
