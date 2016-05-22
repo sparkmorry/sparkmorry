e74c94be023736ba6d70713336bf81c31bc75218
Mengli Xia <morryxia@gmail.com>
2016-05-22 12:44:32 +0800

    blog加入分页

--- sparkmorry/views.py
+++ sparkmorry/views.py
@@ -7,6 +7,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from article.models import Article
 from tag.models import TagArticleRelation, Tag
 
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+
+PAGE_LIMIT = 20
 def home(request):
 	return HttpResponseRedirect("/blog/")
 
@@ -20,7 +25,18 @@ def about(request):
 def blog(request):
 	page='blog'
 	nav='blog'
-	articles = Article.objects.filter(status=0).order_by('-id')[0:15]
+
+	pageIndex = request.GET.get('page', 1)
+	articles = Article.objects.filter(status=0).order_by('-id')
+
+	paginator = Paginator(articles, PAGE_LIMIT)  # 实例化一个分页对象
+	try:
+		articles = paginator.page(pageIndex)  # 获取某页对应的记录
+	except PageNotAnInteger:  # 如果页码不是个整数
+		articles = paginator.page(1)  # 取第一页的记录
+	except EmptyPage:  # 如果页码太大，没有相应的记录
+		articles = paginator.page(paginator.num_pages)  # 取最后一页的记录
+
 	return render_to_response('blog.html', {'page': page, 
 		'articles':articles, 'nav':nav})
 
@@ -53,7 +69,7 @@ def article(request, article_id):
 	nav='blog'
 	try:
 		article = Article.objects.get(id=article_id)
-		tag_relations = TagArticleRelation.objects.filter(article_id=article.id)
+		tag_relations = TagArticleRelation.objects.filter(article_id=article.id, status='0')
 		return render_to_response('article.html', {'page': page, 
 			'article':article, 'tag_list':tag_relations, 'nav':nav})
 	except ObjectDoesNotExist:
